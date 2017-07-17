405218f4f2a78cdd7b5a6ed99a2a050826aee7c7
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-17 12:06:23 -1000

    able to receive requests

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ const fs = require('fs');
 const querystring = require('querystring');
 const http = require('http');
 
-const server = http.createServer((request) => {
-
-request.on('data', (data) => {
-  findFile(data);
-});
-
-function findFile(file){
-  fs.readFile('public/' + file, (err, data) => {
-    var fileContents = data.toString();
+const server = http.createServer((request, response) => {
+  const {headers, method, url} = request;
+  let body = [];
+  request.on('data', (chunk)=> {
+    body.push(chunk);
   });
-}
+
+  request.on('end', ()=> {
+    body = Buffer.concat(body).toString();
+  });
+
+  /*function findFile(file){
+    fs.readFile('public/' + file, (err, data) => {
+      request.write(fileContents);
+      request.end();
+    });
+  }*/
 
 });
 
